6de214e502081bb86204d263405794bbf4849328
Astra-max <waoremaxwel17@gmail.com>
2023-08-11 05:51:49 -0700

    Equality operators

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int n;
+	int ld;
+
+	ld = n % 10;
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (ld > 5)
+	{
+		printf("Last digit of %d is %d and is greater than 5\n", n, ld);
+	}
+	else if (ld == 0)
+	{
+		printf("Last digit of %d is %d and is 0\n", n, ld);
+	}
+	else
+	{
+		printf("Last digit of %d is %d and is less than 0\n", n, ld);
+	}
+	return (0);
+}

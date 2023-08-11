5060e6006f589f2373433beb4413c49dcaf6ac26
Astra-max <waoremaxwel17@gmail.com>
2023-08-11 06:18:29 -0700

    Eliminating o and e

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+* main - Entry point
+* Return: Always 0 (success)
+*/
+int main(void)
+{
+	int n = 97;
+
+	while (n <= 122)
+	{
+		if (n == 101 || n == 111)
+		{
+			n++;
+			continue;
+		}
+		putchar(n);
+		n++;
+	}
+	putchar('\n');
+	return (0);
+}

6e8e033859c89c3face33f253e51f3e22df6f2a8
Astra-max <waoremaxwel17@gmail.com>
2023-08-11 06:50:30 -0700

    printing ascii char in reverse order

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+* main - Entry point
+* Return: Always 0 (success)
+*/
+int main(void)
+{
+	int n = 122;
+
+	while (n >= 97)
+	{
+		putchar(n);
+		n--;
+	}
+	putchar('\n');
+	return (0);
+}

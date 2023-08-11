5c87ef60315c3072ba513196092f9646ebf87709
Astra-max <waoremaxwel17@gmail.com>
2023-08-11 06:03:01 -0700

    Ascii lower case characters

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,10 +1,19 @@
 #include <stdlib.h>
 #include <time.h>
+#include <stdio.h>
 /**
 * main - Entry point
 * Return: Always 0 (success)
 */
 int main(void)
 {
+	int n = 97;
+
+	while (n < 122)
+	{
+		putchar(n);
+		n++;
+	}
+	putchar('\n');
 	return (0);
 }

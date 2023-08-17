cbc3ca87ee1e1e7d517d6df82d060531c2f62505
Astra-max <waoremaxwel17@gmail.com>
2023-08-17 07:16:44 -0700

    Return zero

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * positive_or_negative - Returns positive and negative val
+ * 
+ * @i: Value tested
+ */
+void positive_or_negative(int i)
+{
+	if (i > 0)
+	{
+		printf("%d is positive\n", i);
+	}
+	else if (i < 0)
+	{
+		printf("%d is negative\n", i);
+	}
+	else
+	{
+		printf("%d is zero\n", i);
+	}
+}

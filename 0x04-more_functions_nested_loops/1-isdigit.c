563edc9464abcdb4ebd41b8430348648a7c18145
Astra-max <waoremaxwel17@gmail.com>
2023-08-17 17:35:42 -0700

    1-isdigit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * @x: Test value
+ * Return: 1 for true and viceversa
+ * _isdigit - Returns numbers
+ */
+int _isdigit(int x)
+{
+	if (x >= 48 && x <= 59)
+	{
+		return (1);
+	}
+	return (0);
+}

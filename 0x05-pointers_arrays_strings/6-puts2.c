8aa82d60db947f39b125ca09e073e60378af9f2f
Astra-max <waoremaxwel17@gmail.com>
2023-08-22 01:59:04 -0700

    this funct prints other files

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,20 @@
+#include <string.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * puts2 - Returns other strings not all
+ * @str: string chars tested
+ *
+ */
+
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0; i < strlen(s); i = i + 2)
+	{
+		printf("%c", s[i]);
+	}
+	printf("\n");
+}

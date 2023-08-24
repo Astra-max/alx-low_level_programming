c2bf9d3dde554f19b22582ceadb35156025dd631
Astra-max <waoremaxwel17@gmail.com>
2023-08-24 08:33:29 -0700

    Reversing arrays content

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * reverse_array - Reverses arrays contents
+ * @a: value
+ * @n: Value
+ * Return: 0
+ */
+
+void reverse_array(int *a, int n)
+{
+	int i, j;
+
+	for (i = 0; i < n; i++)
+	{
+		t = a[i];
+		a[i] = a[n];
+		a[n] = t;
+	}
+}

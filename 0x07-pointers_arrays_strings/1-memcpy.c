fbd85301744893979a8fbf686f4875fba0bd7859
Astra-max <waoremaxwel17@gmail.com>
2023-08-28 19:26:36 -0400

    mem copy

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+* _memcpy - copies mem addres
+* @dest: char tested
+* @src: char tested
+* @n: integer
+* Return: Dest
+*/
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+int = r;
+int i = n;
+
+for (; r < i; r++)
+{
+dest[r] = src[r];
+n--;
+}
+return (dest);
+}

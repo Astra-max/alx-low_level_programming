8c4658716fcf3feb967910d1f1fd1c482437b32b
Astra-max <waoremaxwel17@gmail.com>
2023-08-29 01:21:49 +0300

    Fill memory

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _memset - Fills the memory
+ * @s: char tested
+ * @n: unsgned integer
+ * Return: s
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	int i = 0;
+
+	for (; n > 0; i++)
+	{
+		s[i] = b;
+		n--;
+	}
+	return (s);
+}

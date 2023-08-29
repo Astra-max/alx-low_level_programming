f29e69650fff5f5402db0b1ac268dd6a55195bb0
Astra-max <waoremaxwel17@gmail.com>
2023-08-28 20:20:23 -0400

    Print diagsums

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -5,6 +5,7 @@
  * @: integer value
  * @size: integer value
  * Return: 0
+ * @a: an integer
  */
 
 void print_diagsums(int *a, int size)
@@ -20,7 +21,7 @@ void print_diagsums(int *a, int size)
 	}
 	for (y = size - 1; y >= 0; y--)
 	{
-		sum2 += a[y * size + (size -y - 1)];
+		sum2 += a[y * size + (size - y - 1)];
 	}
 	printf("%d, %d\n", sum1, sum2);
 }

370e67379f08566a26178cdad4ae48b14fc95b9b
Astra-max <waoremaxwel17@gmail.com>
2023-09-01 17:38:15 -0400

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+* main - returns programs name
+ * @argc: Total number of args passed
+ * @argv: Array of char passed
+ * Return: 0
+ */
+
+int main(int argc __attribute__((unused)), char *argv[])
+{
+printf("%s\n", argv[0]);
+return (0);
+}

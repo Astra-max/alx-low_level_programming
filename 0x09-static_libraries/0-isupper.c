fa976f37b2569f514d2057630e7a2a5bbb5354ff
Astra-max <waoremaxwel17@gmail.com>
2023-09-01 15:14:01 -0400

    Working with static libraries

---
+++ 0x09-static_libraries/0-isupper.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _isupper - Returns 1 if true
+ * @c: An integer
+ * Return: Always 0
+ *
+ */
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 90)
+	{
+		return (1);
+	}
+	return (0);
+}

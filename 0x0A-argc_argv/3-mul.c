eb3133196e0b80cd326abc42eb7b415e1ff1cb80
Astra-max <waoremaxwel17@gmail.com>
2023-09-01 18:48:55 -0400

    This funct multplys two arg passed

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - multiplies arg passed
+ * Return: -1 if args < 1
+ * @argv: Pointer array that holds args passed
+ * @argc: Number of arguments passed
+ */
+
+int main(int argc __attribute__((unused)), char *argv[])
+{
+int multply, iterator;
+
+for (iterator = 0; iterator < argc; iterator++)
+{
+if (argc <= 1)
+{
+printf("Error\n");
+return (1);
+}
+else
+{
+int number1 = atoi(argv[1]);
+int number2 = atoi(argv[2]);
+
+multply = number1 * number2;
+printf("%d\n", multply);
+break;
+}
+}
+return (0);
+}

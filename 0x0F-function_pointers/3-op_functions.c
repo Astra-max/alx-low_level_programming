2187ef584aa8e36543f7f5f6f2674f0e691b0613
Astra-max <waoremaxwel17@gmail.com>
2023-09-13 18:44:32 -0400

    calculator

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,62 @@
+#include "3-calc.h"
+#include <stdio.h>
+
+/**
+ * op_add - adds
+ * @a: num1
+ * @b: num2
+ * Return: answer
+*/
+
+int op_add(int a, int b)
+{
+return (a + b);
+}
+
+/**
+ * op_sub - substracts two nums
+ * @a: num1
+ * @b: num2
+ * Return: answer
+*/
+
+int op_sub(int a, int b)
+{
+return( a - b);
+}
+
+/**
+ * op_mul - multiplies two num2
+ * @a: num1
+ * @b: num2
+ * Return: answer
+*/
+
+int op_mul(int a, int b)
+{
+return (a * b);
+}
+
+/**
+ * op_div -divides two nums
+ * @a: num1
+ * @b: num2
+ * Return: answer
+*/
+
+int op_div(int a, int b)
+{
+return (a / b);
+}
+
+/**
+ * op_mod - remainder after division
+ * @a: num1
+ * @b: num2
+ * Return: answer
+*/
+
+int op_mod(int a, int b)
+{
+return (a % b);
+}

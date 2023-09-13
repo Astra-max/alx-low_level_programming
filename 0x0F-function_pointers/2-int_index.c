899444623e2ff8aa499c8ffb79583dd5394a3297
Astra-max <waoremaxwel17@gmail.com>
2023-09-13 17:27:01 -0400

    2-int_index.c

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ * int_index - searches for int in array
+ * @array: array to be used
+ * @size: of array
+ * @cmp: pointer to cmp with func pointer
+ * Return: -1
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+int index;
+
+if (array == NULL || cmp == NULL)
+return (-1);
+
+for (index = 0; index < size; index++)
+{
+if (cmp(array[index]) != 0)
+return (index);
+}
+return (-1);
+}

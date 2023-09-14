90acdbfce765a74726a6fd9c729e5f684dbc7f2c
Astra-max <waoremaxwel17@gmail.com>
2023-09-14 18:24:16 -0400

    Format specifiers

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -9,34 +9,34 @@
 void print_all(const char * const format, ...)
 {
 int i, check_status; /* initialisation*/
-char *str;
-va_list spc;
+char *str; /*string char*/
+va_list spc; /*list of args passed*/
 va_start(spc, format); /*variable argument*/
-i = 0;
+i = 0; /*initialized i*/
 
-while (format != NULL && format[i] != '\0')
+while (format != NULL && format[i] != '\0') /*looping thru*/
 {
 switch (format[i])
 {
-case 'i':
+case 'i': /*case i*/
 printf("%d", va_arg(spc, int));
 check_status = 0;
 break;
-case 'f':
+case 'f': /*case f*/
 printf("%f", va_arg(spc, double));
 check_status = 0;
-break;
-case 'c':
+break; /*end of case3*/
+case 'c': /*case c*/
 printf("%c", va_arg(spc, int));
 check_status = 0;
 break;
-case 's':
+case 's': /*case s*/
 str = va_arg(spc, char*);
 if (str == NULL)
 str = "(nil)";
 printf("%s", str);
 break;
-default:
+default: /*Default*/
 check_status = 1;
 break;
 }

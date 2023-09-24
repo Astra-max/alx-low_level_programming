7817a7e4920aa31fecaf702df213a55a7dc0d39d
Astra-max <waoremaxwel17@gmail.com>
2023-09-24 14:49:17 -0400

    coding assembly

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,9 +1,18 @@
-global main
-extern printf
+section .data
+    format db 'Hello, Holberton', 10, 0  ; Added newline character (10)
+
+section .text
+    global main
+    extern printf
+
 main:
-mov edi, format
-xor eax eax
-call printf
-mov eax, 0
-ret
-format:db 'Hello, Holberton\n',0
+    sub rsp, 8              ; Align stack (assuming you are using GCC)
+    mov rdi, format         ; Load the format string address into rdi
+    call printf            ; Call printf
+    add rsp, 8              ; Restore stack
+    mov rax, 60             ; syscall number for exit (64-bit)
+    xor rdi, rdi            ; status: 0
+    syscall                ; Invoke syscall to exit
+
+section .note.GNU-stack
+

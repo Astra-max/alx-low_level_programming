536d1bf674b7c1ea6d730dab49405879cecefae5
Astra-max <waoremaxwel17@gmail.com>
2023-09-28 21:10:56 -0400

    Calculates bits

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,4 +1,18 @@
 #include "main.h"
 
 /**
- * get_bit - ta
+ * get_bit - takes val & Return bits
+ * @n: val tested
+ * @index: indx
+ * Return: index
+ */
+
+int get_bit(unsigned long int n, unsigned int index)
+{
+int val;
+
+if (index > 63)
+return (-1);
+val = (n >> index) & 1;
+return (val);
+}

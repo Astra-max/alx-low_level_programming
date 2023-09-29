1eb09db4757d8e9a906976e986b7edee9f868d63
Astra-max <waoremaxwel17@gmail.com>
2023-09-28 21:34:59 -0400

    Get endians

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,14 @@
+ #include "main.h"
+
+/**
+ * get_endianness - get endian
+ * Returns int but takes nothing
+ * Return: c
+ */
+
+int get_endianness(void)
+{
+unsigned int me = 1;
+char *c = (char *) & me;
+return (*c);
+}

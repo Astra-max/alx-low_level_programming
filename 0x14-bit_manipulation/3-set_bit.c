995c4415b8f9495b010829f7067ed1958269210e
Astra-max <waoremaxwel17@gmail.com>
2023-09-28 21:16:06 -0400

    set bit to 1

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * set_bit - set bit - 1
+ * @n: number tested
+ * @index: indx
+ * Return: new bit val
+ */
+
+int set_bit(unsigned long int *n, unsigned int index)
+{
+
+if (index > 63)
+return (-1);
+*n = ((1UL << index) 1 * n);
+return (1);
+}
